edb7a51a2f6a4c36204172f06f6868d2177ef4f7
Tert-butyllithium <lihaonan1105@gmail.com>
2020-05-16 12:04:12 +0800

    add autocomplete for purchase

--- layouts/default.vue
+++ layouts/default.vue
@@ -59,7 +59,7 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
@@ -67,7 +67,7 @@
       </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn v-show="islogin && !isloginLoading" icon @click="loginout">
--- pages/purchase.vue
+++ pages/purchase.vue
@@ -3,13 +3,136 @@
     <v-flex class="text-center">
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
+        <v-card color="blue lighten-2">
+          <v-card-title class="headline blue lighten-3">
+            Search for Public APIs
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-autocomplete
+                v-model="model"
+                :items="items"
+                :loading="isLoading"
+                :search-input.sync="deptSearch"
+                color="white"
+                hide-no-data
+                hide-selected
+                item-text="Description"
+                item-value="API"
+                label="Public APIs"
+                placeholder="Start typing to Search"
+                prepend-icon="mdi-airplane-takeoff"
+                return-object
+              ></v-autocomplete>
+
+              <v-autocomplete
+                v-model="model"
+                :items="items"
+                :loading="isLoading"
+                :search-input.sync="termiSearch"
+                color="white"
+                hide-no-data
+                hide-selected
+                item-text="Description"
+                item-value="API"
+                label="Public APIs"
+                placeholder="Start typing to Search"
+                prepend-icon="mdi-airplane-landing"
+                return-object
+              ></v-autocomplete>
+            </v-row>
+          </v-card-text>
+          <!-- <v-divider></v-divider> -->
+          <v-divider></v-divider>
+          <v-expand-transition>
+            <v-list v-if="model" class="red lighten-3">
+              <v-list-item v-for="(field, i) in fields" :key="i">
+                <v-list-item-content>
+                  <v-list-item-title v-text="field.value"></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-text="field.key"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-expand-transition>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn :disabled="!model" @click="model = null">
+              Clear
+              <v-icon right>mdi-close-circle</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </blockquote>
     </v-flex>
   </v-layout>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      descriptionLimit: 60,
+      entries: [],
+      isLoading: false,
+      model: null,
+      // search: null
+      deptSearch: null,
+      termiSearch: null
+    }
+  },
+  computed: {
+    fields() {
+      if (!this.model) return []
+
+      return Object.keys(this.model).map((key) => {
+        return {
+          key,
+          value: this.model[key] || 'n/a'
+        }
+      })
+    },
+    items() {
+      return this.entries.map((entry) => {
+        const Description =
+          entry.Description.length > this.descriptionLimit
+            ? entry.Description.slice(0, this.descriptionLimit) + '...'
+            : entry.Description
+
+        return Object.assign({}, entry, { Description })
+      })
+    }
+  },
+
+  watch: {
+    search(val) {
+      // Items have already been loaded
+      if (this.items.length > 0) return
+
+      // Items have already been requested
+      if (this.isLoading) return
+
+      this.isLoading = true
+
+      // Lazily load input items
+      fetch('/api/station')
+        .then((res) => res.json())
+        .then((res) => {
+          // eslint-disable-next-line camelcase
+          const { result_cnt, result } = res
+          // eslint-disable-next-line camelcase
+          this.count = result_cnt
+          this.entries = result
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.log(err)
+        })
+        .finally(() => (this.isLoading = false))
+      // this.entries = this.names
+      // this.count = this.names.length
+      // this.isLoading = false
+    }
+  }
+}
+</script>
